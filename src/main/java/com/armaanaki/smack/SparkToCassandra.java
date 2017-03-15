3d4f1632462d8dfffbf7e3a2cb1a931c69872a8b
Armaan Esfahani <armaanaki@gmail.com>
2017-03-15 14:18:17 -0400

    Seperated project to multiple parts & allow command line parameters to set important values Signed-off-by: Armaan Esfahani <armaanaki@gmail.com>

--- src/main/java/com/armaanaki/smack/SparkToCassandra.java
+++ src/main/java/com/armaanaki/smack/SparkToCassandra.java
@@ -1,4 +1,4 @@
-package com.armaanaki.smack.spark;
+package com.armaanaki.smack;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -9,40 +9,39 @@ import java.util.Set;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
 
-import scala.Tuple2;
-
-import com.armaanaki.smack.tweet.*;
+import com.datastax.spark.connector.japi.CassandraJavaUtil;
+import com.datastax.spark.connector.japi.CassandraStreamingJavaUtil;
 
 public class SparkToCassandra {
 	public static void main(String[] args) throws Exception{
-		if (args.length < 4 && false) {
+		if (args.length < 4) {
 			System.err.println("Usage: SparkTest <kafka brokers> <topics> <cassandra host> <cassandra port>");
 		    System.exit(1);
 		}
 		
-		String brokers = "192.168.194.194:9092";//args[0];
-		String topics = "tweets1";//args[1];
+		String brokers = args[0];
+		String topics = args[1];
 		
 		Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
 		
 		SparkConf sparkConf = new SparkConf().setAppName("SparkTest")
-				.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer"); 
-				//.set("spark.cassandra.connection.host", args[4])
-				//.set("spark.cassandra.connection.port", args[5]);
+				.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer") 
+				.set("spark.cassandra.connection.host", args[2])
+				.set("spark.cassandra.connection.port", args[3]);
 		
 		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(1));
 		
 		 Map <String, Object> kafkaParams = new HashMap<>();
 		 kafkaParams.put("bootstrap.servers", brokers);
-		 //kafkaParams.put("auto.offset.reset", "smallest");
 		 kafkaParams.put("key.deserializer", StringDeserializer.class);
 		 kafkaParams.put("value.deserializer", TweetDeserializer.class);
 		 kafkaParams.put("group.id", "use_a_separate_group_id_for_each_stream");
@@ -52,16 +51,18 @@ public class SparkToCassandra {
 				 LocationStrategies.PreferConsistent(),
 				 ConsumerStrategies.<String, Tweet>Subscribe(topicsSet, kafkaParams)
 				);
-		tweets.mapToPair(
-				 new PairFunction<ConsumerRecord<String, Tweet>, String, Tweet>() {
+		 
+		 
+		JavaDStream<Tweet> tweetStream= tweets.map(
+				 new Function<ConsumerRecord<String, Tweet>, Tweet>() {
 					 @Override
-					 public Tuple2<String, Tweet>call(ConsumerRecord<String, Tweet> record) {
-						 System.out.println(record.value());
-						 return new Tuple2<>(record.key(), record.value());
+					 public Tweet call(ConsumerRecord<String, Tweet> record) {
+						 return record.value();
 					 }
-				 }).print();
-		 //tweets.print();
-		 jssc.start();
-		 jssc.awaitTermination();
+				 });
+		tweetStream.print();
+		CassandraStreamingJavaUtil.javaFunctions(tweetStream).writerBuilder("dcos", "tweets", CassandraJavaUtil.mapToRow(Tweet.class)).saveToCassandra();
+		jssc.start();
+		jssc.awaitTermination();
 	}
 }
